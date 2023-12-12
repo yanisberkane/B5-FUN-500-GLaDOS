e9ea0aea494ca4800e8d7a0adc5bcaeb396b7abb
Rask <afraieda@gmail.com>
2023-12-12 17:36:00 +0100

    test: ci w/ failure test

--- test/TestMain.hs
+++ test/TestMain.hs
@@ -7,7 +7,7 @@ import ErrorHandler (ErrorType(..))
 testDefineVariable :: Test
 testDefineVariable = TestCase $ do
     let env = Map.empty
-    let ast = sExprToAst (SList [SSymbol "define", SSymbol "foo", SInt 21])
+    let ast = sExprToAst (SList [SSymbol "define", SSymbol "feo", SInt 21])
     let expected = Right (Define (SSymbol "foo") (SInt 21) (AstInt 21), Map.fromList [("foo", AstInt 21)])
     assertEqual "Define variable foo to 21" expected (evaluate env ast)
 
