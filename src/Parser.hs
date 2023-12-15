9e03950ec0397b3a639f40100e60ada3812683ba
NotAJunkie2 <artyom.tillon@epitech.eu>
2023-12-15 20:34:58 +0100

    feat: added <>= characters

--- src/Parser.hs
+++ src/Parser.hs
@@ -101,7 +101,7 @@ parseQuotedSymbol = do parseChar '"'
                        return str
 
 parseSymbol :: Parser String
-parseSymbol = parseSome $ parseOr (parseOr (parseAnyChar ['a'..'z']) (parseAnyChar ['A'..'Z'])) (parseAnyChar "*+-/%!?")
+parseSymbol = parseSome $ parseOr (parseOr (parseAnyChar ['a'..'z']) (parseAnyChar ['A'..'Z'])) (parseAnyChar "*+-/%!?<>=")
 
 parseWhiteSpace :: Parser String
 parseWhiteSpace = parseSome $ parseAnyChar " \t\n"
