4739db15025967a081ce7ba2a3ef00dbe1547027
Aurélien Leclercq <aurelien.leclercq@epitech.eu>
2023-12-15 20:46:19 +0100

    feat: changed operators, more optimized now

--- src/Operators.hs
+++ src/Operators.hs
@@ -6,37 +6,25 @@ import ErrorHandler (ErrorType(..))
 type EvaluateFunc = Env -> Ast -> Either ErrorType (Ast, Env)
 
 add :: EvaluateFunc -> Env -> [Ast] -> Either ErrorType (Ast, Env)
-add evaluate env args = do
-    evaluatedArgs <- mapM (evaluate env) args
-    let ints = mapM (\(ast, _) -> astToInt ast) evaluatedArgs
-    case ints of
-        Right ns -> Right (AstInt $ sum ns, env)
-        Left err -> Left err
+add evaluate env args = arithmeticOp evaluate env args sum
 
 subtract :: EvaluateFunc -> Env -> [Ast] -> Either ErrorType (Ast, Env)
-subtract evaluate env (x:xs) = do
-    evaluatedArgs <- mapM (evaluate env) (x:xs)
-    let ints = mapM (\(ast, _) -> astToInt ast) evaluatedArgs
-    case ints of
-        Right (n:ns) -> Right (AstInt $ foldl (-) n ns, env)
-        Right [] -> Left $ RuntimeError "Subtraction needs at least one argument"
-        Left err -> Left err
+subtract evaluate env [x] = Left $ RuntimeError "Subtraction requires at least two arguments"
+subtract evaluate env (x:xs) = arithmeticOp evaluate env (x:xs) (\ns -> foldl (-) (head ns) (tail ns))
 
 multiply :: EvaluateFunc -> Env -> [Ast] -> Either ErrorType (Ast, Env)
-multiply evaluate env args = do
+multiply evaluate env args = arithmeticOp evaluate env args product
+
+divide :: EvaluateFunc -> Env -> [Ast] -> Either ErrorType (Ast, Env)
+divide evaluate env [x] = Left $ RuntimeError "Division requires at least two arguments"
+divide evaluate env (x:xs) = arithmeticOp evaluate env (x:xs) (\ns -> foldl div (head ns) (tail ns))
+
+arithmeticOp :: EvaluateFunc -> Env -> [Ast] -> ([Int] -> Int) -> Either ErrorType (Ast, Env)
+arithmeticOp evaluate env args op = do
     evaluatedArgs <- mapM (evaluate env) args
     let ints = mapM (\(ast, _) -> astToInt ast) evaluatedArgs
     case ints of
-        Right ns -> Right (AstInt $ product ns, env)
-        Left err -> Left err
-
-divide :: EvaluateFunc -> Env -> [Ast] -> Either ErrorType (Ast, Env)
-divide evaluate env (x:xs) = do
-    evaluatedArgs <- mapM (evaluate env) (x:xs)
-    let ints = mapM (\(ast, _) -> astToInt ast) evaluatedArgs
-    case ints of
-        Right (n:ns) -> Right (AstInt $ foldl (div) n ns, env)
-        Right [] -> Left $ RuntimeError "Division needs at least one argument"
+        Right ns -> Right (AstInt $ op ns, env)
         Left err -> Left err
 
 astToInt :: Ast -> Either ErrorType Int
