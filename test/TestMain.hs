d0db9c9ec73cf1d17f27d7301d1421f72a960c0b
Yanis Berkane <yanisberkane@Yaniss-MacBook-Air.local>
2023-12-18 17:09:25 +0100

    [TEST] added Unit tests for Parser module

--- test/TestMain.hs
+++ test/TestMain.hs
@@ -3,11 +3,227 @@ import Interpreter (evaluate)
 import Types (Ast(..), Env)
 import qualified Data.Map as Map
 import ErrorHandler (ErrorType(..))
+import Parser
+import Control.Applicative
 
 evaluateAst :: Env -> Maybe Ast -> Either ErrorType (Ast, Env)
 evaluateAst env maybeAst = case maybeAst of
     Just ast -> evaluate env ast
     Nothing -> Left $ RuntimeError "Failed to parse AST"
+
+-- Tests for the parser
+
+testAlternative :: Test
+testAlternative = TestCase $ do
+    let parser = runParser (parseChar 'a' <|> parseChar 'b') "abcd"
+    let expected = Just ('a', "bcd")
+    assertEqual "parseChar 'a' <|> parseChar 'b' on 'abcd'" expected (parser)
+
+testEmptyAlternative :: Test
+testEmptyAlternative = TestCase $ do
+    let parser = runParser (parseChar 'a' <|> parseChar 'b' <|> empty) "cdef"
+    let expected = Nothing
+    assertEqual "parseChar 'a' <|> parseChar 'b' on 'cdef'" expected (parser)
+
+testParseChar :: Test
+testParseChar = TestCase $ do
+    let parser = runParser (parseChar 'a') "abcd"
+    let expected = Just ('a', "bcd")
+    assertEqual "parseChar 'a' on 'abcd'" expected (parser)
+
+testParseCharFail :: Test
+testParseCharFail = TestCase $ do
+    let parser = runParser (parseChar 'a') "bcde"
+    let expected = Nothing
+    assertEqual "parseChar 'a' on 'bcde'" expected (parser)
+
+testParseAnyChar :: Test
+testParseAnyChar = TestCase $ do
+    let parser = runParser (parseAnyChar "bca") "cdef"
+    let expected = Just ('c', "def")
+    assertEqual "parseAnyChar \"bca\" on \"cdef\"" expected (parser)
+
+testParseAnyCharFail :: Test
+testParseAnyCharFail = TestCase $ do
+    let parser = runParser (parseAnyChar "bca") "defg"
+    let expected = Nothing
+    assertEqual "parseAnyChar \"bca\" on \"defg\"" expected (parser)
+
+testParseOr :: Test
+testParseOr = TestCase $ do
+    let parser = runParser (parseOr (parseChar 'a') (parseChar 'b')) "abcd"
+    let expected = Just ('a', "bcd")
+    assertEqual "parseOr (parseChar 'a') (parseChar 'b') on 'abcd'" expected (parser)
+
+testParseOr2 :: Test
+testParseOr2 = TestCase $ do
+    let parser = runParser (parseOr (parseChar 'a') (parseChar 'b')) "bcde"
+    let expected = Just ('b', "cde")
+    assertEqual "parseOr (parseChar 'a') (parseChar 'b') on 'bcde'" expected (parser)
+
+testParseAnd :: Test
+testParseAnd = TestCase $ do
+    let parser = runParser (parseAnd (parseChar 'a') (parseChar 'b')) "abcd"
+    let expected = Just (('a', 'b'), "cd")
+    assertEqual "parseAnd (parseChar 'a') (parseChar 'b') on 'abcd'" expected (parser)
+
+testParseAndFail :: Test
+testParseAndFail = TestCase $ do
+    let parser = runParser (parseAnd (parseChar 'a') (parseChar 'b')) "acde"
+    let expected = Nothing
+    assertEqual "parseAnd (parseChar 'a') (parseChar 'b') on 'acde'" expected (parser)
+
+testParseAndFail2 :: Test
+testParseAndFail2 = TestCase $ do
+    let parser = runParser (parseAnd (parseChar 'z') (parseChar 'b')) "bcde"
+    let expected = Nothing
+    assertEqual "parseAnd (parseChar 'a') (parseChar 'b') on 'bcde'" expected (parser)
+
+testParseAndWith :: Test
+testParseAndWith = TestCase $ do
+    let parser = runParser (parseAndWith (\x y -> [x, y]) (parseChar 'a') (parseChar 'b')) "abcd"
+    let expected = Just ("ab", "cd")
+    assertEqual "parseAndWith (\\x y -> [x, y]) (parseChar 'a') (parseChar 'b') on 'abcd'" expected (parser)
+
+testParseAndWithFail :: Test
+testParseAndWithFail = TestCase $ do
+    let parser = runParser (parseAndWith (\x y -> [x, y]) (parseChar 'a') (parseChar 'b')) "acde"
+    let expected = Nothing
+    assertEqual "parseAndWith (\\x y -> [x, y]) (parseChar 'a') (parseChar 'b') on 'acde'" expected (parser)
+
+testParseAndWithFail2 :: Test
+testParseAndWithFail2 = TestCase $ do
+    let parser = runParser (parseAndWith (\x y -> [x, y]) (parseChar 'z') (parseChar 'b')) "bcde"
+    let expected = Nothing
+    assertEqual "parseAndWith (\\x y -> [x, y]) (parseChar 'a') (parseChar 'b') on 'bcde'" expected (parser)
+
+testParseMany :: Test
+testParseMany = TestCase $ do
+    let parser = runParser (parseMany (parseChar 'a')) "aaabcd"
+    let expected = Just ("aaa", "bcd")
+    assertEqual "parseMany (parseChar 'a') on 'aaabcd'" expected (parser)
+
+testParseManyFail :: Test
+testParseManyFail = TestCase $ do
+    let parser = runParser (parseMany (parseChar 'a')) "bcde"
+    let expected = Just ("", "bcde")
+    assertEqual "parseMany (parseChar 'a') on 'bcde'" expected (parser)
+
+testParseManyFail2 :: Test
+testParseManyFail2 = TestCase $ do
+    let parser = runParser (parseMany (parseChar ' ')) ""
+    let expected = Just ("", "")
+    assertEqual "parseMany (parseChar ' ') on ''" expected (parser)
+
+testParseSome :: Test
+testParseSome = TestCase $ do
+    let parser = runParser (parseSome (parseChar 'a')) "aaabcd"
+    let expected = Just ("aaa", "bcd")
+    assertEqual "parseSome (parseChar 'a') on 'aaabcd'" expected (parser)
+
+testParseSomeFail :: Test
+testParseSomeFail = TestCase $ do
+    let parser = runParser (parseSome (parseChar 'a')) "bcde"
+    let expected = Nothing
+    assertEqual "parseSome (parseChar 'a') on 'bcde'" expected (parser)
+
+testParseSomeFail2 :: Test
+testParseSomeFail2 = TestCase $ do
+    let parser = runParser (parseSome (parseChar 'z')) "testMessage"
+    let expected = Nothing
+    assertEqual "parseSome (parseChar 'z') on 'testMessage'" expected (parser)
+
+testParseNoneOf :: Test
+testParseNoneOf = TestCase $ do
+    let parser = runParser (parseNoneOf "abc") "defg"
+    let expected = Just ('d', "efg")
+    assertEqual "parseNoneOf \"abc\" on \"defg\"" expected (parser)
+
+testParseNoneOfFail :: Test
+testParseNoneOfFail = TestCase $ do
+    let parser = runParser (parseNoneOf "abc") "abc"
+    let expected = Nothing
+    assertEqual "parseNoneOf \"abc\" on \"abc\"" expected (parser)
+
+testParseUInt :: Test
+testParseUInt = TestCase $ do
+    let parser = runParser parseUInt "1234"
+    let expected = Just (1234, "")
+    assertEqual "parseUInt on \"1234\"" expected (parser)
+
+testParseUIntFail :: Test
+testParseUIntFail = TestCase $ do
+    let parser = runParser parseUInt "abc"
+    let expected = Nothing
+    assertEqual "parseUInt on \"abc\"" expected (parser)
+
+testParseInt :: Test
+testParseInt = TestCase $ do
+    let parser = runParser parseInt "-1234"
+    let expected = Just (-1234, "")
+    assertEqual "parseInt on \"-1234\"" expected (parser)
+
+testParseInt2 :: Test
+testParseInt2 = TestCase $ do
+    let parser = runParser parseInt "+1234"
+    let expected = Just (1234, "")
+    assertEqual "parseInt on \"+1234\"" expected (parser)
+
+testParseIntFail :: Test
+testParseIntFail = TestCase $ do
+    let parser = runParser parseInt "abc"
+    let expected = Nothing
+    assertEqual "parseInt on \"abc\"" expected (parser)
+
+testParseQuotedSymbol :: Test
+testParseQuotedSymbol = TestCase $ do
+    let parser = runParser parseQuotedSymbol "\"foo\""
+    let expected = Just ("foo", "")
+    assertEqual "parseQuotedSymbol on \"foo\"" expected (parser)
+
+testParseQuotedSymbolFail :: Test
+testParseQuotedSymbolFail = TestCase $ do
+    let parser = runParser parseQuotedSymbol "foo"
+    let expected = Nothing
+    assertEqual "parseQuotedSymbol on \"foo\"" expected (parser)
+
+testParseSymbol :: Test
+testParseSymbol = TestCase $ do
+    let parser = runParser parseSymbol "foo"
+    let expected = Just ("foo", "")
+    assertEqual "parseSymbol on \"foo\"" expected (parser)
+
+testParseSymbolFail :: Test
+testParseSymbolFail = TestCase $ do
+    let parser = runParser parseSymbol "\"foo\""
+    let expected = Nothing
+    assertEqual "parseSymbol on \"foo\"" expected (parser)
+
+testParseWhiteSpace :: Test
+testParseWhiteSpace = TestCase $ do
+    let parser = runParser parseWhiteSpace "  foo"
+    let expected = Just ("  ", "foo")
+    assertEqual "parseWhiteSpace on \"  foo\"" expected (parser)
+
+testParseWhiteSpaceFail :: Test
+testParseWhiteSpaceFail = TestCase $ do
+    let parser = runParser parseWhiteSpace "foo"
+    let expected = Nothing
+    assertEqual "parseWhiteSpace on \"foo\"" expected (parser)
+
+-- testParseString :: Test
+-- testParseString = TestCase $ do
+--     let parser = runParser parseString "\"foo\""
+--     let expected = Just ("foo", "")
+--     assertEqual "parseString on \"foo\"" expected (parser)
+
+-- testParseStringFail :: Test
+-- testParseStringFail = TestCase $ do
+--     let parser = runParser parseString "foo"
+--     let expected = Nothing
+--     assertEqual "parseString on \"foo\"" expected (parser)
+
+-- Tests for the interpreter
 
 testDivision :: Test
 testDivision = TestCase $ do
@@ -124,22 +340,57 @@ testModulo = TestCase $ do
 
 tests :: Test
 tests = TestList [
-                   TestLabel "testDivision" testDivision,
-                   TestLabel "testBasicAddition" testBasicAddition,
-                   TestLabel "testBasicSubtraction" testBasicSubtraction,
-                   TestLabel "testDefineAndUseVariable" testDefineAndUseVariable,
-                   TestLabel "testUndefinedVariableError" testUndefinedVariableError,
-                   TestLabel "testLambda1" testLambda1,
-                   TestLabel "testLambda2" testLambda2,
-                   TestLabel "testIf1" testIf1,
-                   TestLabel "testIf2" testIf2,
-                   TestLabel "testIf3" testIf3,
-                   TestLabel "testBuiltins1" testBuiltins1,
-                   TestLabel "testBuiltins2" testBuiltins2,
-                   TestLabel "testIfWithAddition" testIfWithAddition,
-                   TestLabel "testIfWithSubtraction" testIfWithSubtraction,
-                   TestLabel "testModulo" testModulo
-                 ]
+                    TestLabel "testDivision" testDivision,
+                    TestLabel "testBasicAddition" testBasicAddition,
+                    TestLabel "testBasicSubtraction" testBasicSubtraction,
+                    TestLabel "testDefineAndUseVariable" testDefineAndUseVariable,
+                    TestLabel "testUndefinedVariableError" testUndefinedVariableError,
+                    TestLabel "testLambda1" testLambda1,
+                    TestLabel "testLambda2" testLambda2,
+                    TestLabel "testIf1" testIf1,
+                    TestLabel "testIf2" testIf2,
+                    TestLabel "testIf3" testIf3,
+                    TestLabel "testBuiltins1" testBuiltins1,
+                    TestLabel "testBuiltins2" testBuiltins2,
+                    TestLabel "testIfWithAddition" testIfWithAddition,
+                    TestLabel "testIfWithSubtraction" testIfWithSubtraction,
+                    TestLabel "testModulo" testModulo,
+                    TestLabel "testAlternative" testAlternative,
+                    TestLabel "testEmptyAlternative" testEmptyAlternative,
+                    TestLabel "testParseChar" testParseChar,
+                    TestLabel "testParseCharFail" testParseCharFail,
+                    TestLabel "testParseAnyChar" testParseAnyChar,
+                    TestLabel "testParseAnyCharFail" testParseAnyCharFail,
+                    TestLabel "testParseOr" testParseOr,
+                    TestLabel "testParseOr2" testParseOr2,
+                    TestLabel "testParseAnd" testParseAnd,
+                    TestLabel "testParseAndFail" testParseAndFail,
+                    TestLabel "testParseAndFail2" testParseAndFail2,
+                    TestLabel "testParseAndWith" testParseAndWith,
+                    TestLabel "testParseAndWithFail" testParseAndWithFail,
+                    TestLabel "testParseAndWithFail2" testParseAndWithFail2,
+                    TestLabel "testParseMany" testParseMany,
+                    TestLabel "testParseManyFail" testParseManyFail,
+                    TestLabel "testParseManyFail2" testParseManyFail2,
+                    TestLabel "testParseSome" testParseSome,
+                    TestLabel "testParseSomeFail" testParseSomeFail,
+                    TestLabel "testParseSomeFail2" testParseSomeFail2,
+                    TestLabel "testParseNoneOf" testParseNoneOf,
+                    TestLabel "testParseNoneOfFail" testParseNoneOfFail,
+                    TestLabel "testParseUInt" testParseUInt,
+                    TestLabel "testParseUIntFail" testParseUIntFail,
+                    TestLabel "testParseInt" testParseInt,
+                    TestLabel "testParseInt2" testParseInt2,
+                    TestLabel "testParseIntFail" testParseIntFail,
+                    TestLabel "testParseQuotedSymbol" testParseQuotedSymbol,
+                    TestLabel "testParseQuotedSymbolFail" testParseQuotedSymbolFail,
+                    TestLabel "testParseSymbol" testParseSymbol,
+                    TestLabel "testParseSymbolFail" testParseSymbolFail,
+                    TestLabel "testParseWhiteSpace" testParseWhiteSpace,
+                    TestLabel "testParseWhiteSpaceFail" testParseWhiteSpaceFail
+                    -- TestLabel "testParseString" testParseString
+                    -- TestLabel "testParseStringFail" testParseStringFail
+                ]
 
 main :: IO Counts
 main = runTestTT tests
