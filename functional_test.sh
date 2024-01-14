f6816e35cd88d0a5242a51516d335770af0a1081
Marius Lacomblez <marius.lacomblez@epitech.eu>
2024-01-14 16:53:11 +0100

    [ADD] added all tests for glados-vm, some in comments as I'm waiting for a certain fix to be pushed

--- functional_test.sh
+++ functional_test.sh
@@ -9,6 +9,11 @@ GREEN='\033[0;32m'
 perform_test() {
     input_file="$1"
     expected_output_file="$2"
+    isvm="$3"
+
+    if [ "$isvm" = "vm" ]; then
+        BINARY_PATH="./glados-vm"
+    fi
 
     # Run the binary with the input file
     actual_output=$("$BINARY_PATH" "$input_file" 2>&1)
@@ -56,6 +61,48 @@ perform_test "Testfile/test_lambda/lambda4_ss.ccs" "Testfile/outputs/compiler/la
 perform_test "Testfile/test_math_op/math_op1.ccs" "Testfile/outputs/compiler/math_op1.txt"
 perform_test "Testfile/test_math_op/operator1.ccs" "Testfile/outputs/compiler/operator1.txt"
 perform_test "Testfile/test_math_op/operator2.ccs" "Testfile/outputs/compiler/operator2.txt"
+
+mv *.dz Testfile/outputs/compiled/
+
+echo "-----------------------"
+echo -e "${GREEN}COMPILER TESTS PASSED${NC}"
+echo "-----------------------"
+echo "VM TESTS STARTING"
+echo "-----------------------"
+
+# VM TESTS
+
+perform_test "Testfile/outputs/compiled/assign1.dz" "Testfile/outputs/vmoutputs/assign1.txt" "vm"
+perform_test "Testfile/outputs/compiled/assign2.dz" "Testfile/outputs/vmoutputs/assign2.txt" "vm"
+perform_test "Testfile/outputs/compiled/assign3.dz" "Testfile/outputs/vmoutputs/assign3.txt" "vm"
+
+perform_test "Testfile/outputs/compiled/define1.dz" "Testfile/outputs/vmoutputs/define1.txt" "vm"
+perform_test "Testfile/outputs/compiled/define2.dz" "Testfile/outputs/vmoutputs/define2.txt" "vm"
+# perform_test "Testfile/outputs/compiled/define3.dz" "Testfile/outputs/vmoutputs/define3.txt" "vm"
+perform_test "Testfile/outputs/compiled/define4.dz" "Testfile/outputs/vmoutputs/define4.txt" "vm"
+perform_test "Testfile/outputs/compiled/define5.dz" "Testfile/outputs/vmoutputs/define5.txt" "vm"
+
+perform_test "Testfile/outputs/compiled/if1.dz" "Testfile/outputs/vmoutputs/if1.txt" "vm"
+perform_test "Testfile/outputs/compiled/if2.dz" "Testfile/outputs/vmoutputs/if2.txt" "vm"
+perform_test "Testfile/outputs/compiled/if3.dz" "Testfile/outputs/vmoutputs/if3.txt" "vm"
+perform_test "Testfile/outputs/compiled/if4.dz" "Testfile/outputs/vmoutputs/if4.txt" "vm"
+perform_test "Testfile/outputs/compiled/if5.dz" "Testfile/outputs/vmoutputs/if5.txt" "vm"
+perform_test "Testfile/outputs/compiled/if6.dz" "Testfile/outputs/vmoutputs/if6.txt" "vm"
+
+perform_test "Testfile/outputs/compiled/call1.dz" "Testfile/outputs/vmoutputs/call1.txt" "vm"
+# perform_test "Testfile/outputs/compiled/call2.dz" "Testfile/outputs/vmoutputs/call2.txt" "vm"
+# perform_test "Testfile/outputs/compiled/call3.dz" "Testfile/outputs/vmoutputs/call3.txt" "vm"
+
+perform_test "Testfile/outputs/compiled/lambda1.dz" "Testfile/outputs/vmoutputs/lambda1.txt" "vm"
+perform_test "Testfile/outputs/compiled/lambda2.dz" "Testfile/outputs/vmoutputs/lambda2.txt" "vm"
+perform_test "Testfile/outputs/compiled/lambda3.dz" "Testfile/outputs/vmoutputs/lambda3.txt" "vm"
+perform_test "Testfile/outputs/compiled/lambda4_ss.dz" "Testfile/outputs/vmoutputs/lambda4_ss.txt" "vm"
+
+# perform_test "Testfile/outputs/compiled/math_op1.dz" "Testfile/outputs/vmoutputs/math_op1.txt" "vm"
+perform_test "Testfile/outputs/compiled/operator1.dz" "Testfile/outputs/vmoutputs/operator1.txt" "vm"
+perform_test "Testfile/outputs/compiled/operator2.dz" "Testfile/outputs/vmoutputs/operator2.txt" "vm"
+
+# perform_test "test" "test" "vm"
 # perform_test "test_input2.txt" "expected_output2.txt"
 # perform_test "test_input3.txt" "expected_output3.txt"
 
